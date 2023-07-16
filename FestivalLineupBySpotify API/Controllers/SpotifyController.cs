935d478135c627163837090116b38dd496436529
Alon Dean <alondean@gmail.com>
2023-07-16 04:25:12 +0300

    Also return the score to the user, in addition to the url

--- FestivalLineupBySpotify API/Controllers/SpotifyController.cs
+++ FestivalLineupBySpotify API/Controllers/SpotifyController.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Text.RegularExpressions;
 using FestivalLineupBySpotify_API.Services;
 using Microsoft.AspNetCore.Diagnostics;
@@ -18,10 +18,10 @@ namespace FestivalLineupBySpotify_API.Controllers
         }
         
         [HttpGet(Name = "Test Sppotify API call")]
-        public async Task<string> GeneratePersonalizedClashFindersURL(string festivalName)
+        public async Task<ClashFindersFavoritesResult> GeneratePersonalizedClashFindersURL(string festivalName)
         {
-            var result = await SpotifyService.GenerateClashFindersFavoritesResult(Request, festivalName);
-            return result.Url;
+            var result = await _spotifyService.GenerateClashFindersFavoritesResult(Request, festivalName);
+            return result;
         }
     }
 
