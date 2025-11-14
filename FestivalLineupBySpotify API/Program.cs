97df7d808446ba9062baafb4ea0aa1700a850fba
Alonzzzo2 <alondean@gmail.com>
2025-11-14 19:25:18 +0200

    adding the fe to cors

--- FestivalLineupBySpotify API/Program.cs
+++ FestivalLineupBySpotify API/Program.cs
@@ -33,7 +33,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173", "http://localhost:3000")
+        policy.WithOrigins("http://localhost:5173", "http://localhost:3000", "https://festivallineupbyspotify-fe.onrender.com")
               .AllowAnyMethod()
               .AllowAnyHeader()
               .AllowCredentials();
